cf513be56bf5749a010c6a59490bc5116157f7c7
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-10 09:27:57 -0300

    tipo any

--- src/type-annotation/aula1.ts
+++ src/type-annotation/aula1.ts
@@ -7,4 +7,22 @@ let simbolo:symbol = Symbol('qualquer-simbolo');
 
 
 //arrays
-let arrayDeNumeros
+let arrayDeNumeros:Array<number> = [1,2,3];
+let arrayDeNumeros2:number[] = [1,2,3];
+let arrayDeStrings:Array<string> = ['1','2','3'];
+
+//objetos
+let pessoa: {nome:string, idade:number, adulto?:boolean} = {
+  nome:"Otávio",
+  idade:22,
+  adulto: true
+}
+
+//funções
+function soma(x:number,y:number):number{
+  return x + y
+}
+const result = soma(2,2)
+
+const soma2: (x:number, y:number) => number = (x,y) => x + y;
+
---
+++ src/type-annotation/aula2.ts
@@ -0,0 +1,9 @@
+//implícito
+function showMessage(msg){
+  return msg;
+}
+
+// explícito
+function showMessage2(msg:any){
+  return msg;
+}
